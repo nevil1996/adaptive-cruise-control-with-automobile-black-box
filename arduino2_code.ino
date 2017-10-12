008dc802b2a9d5d1340bdd34bcf4e63a2c790a19
Nevil Rayan Serrao <nevilrayans@gmail.com>
2017-10-12 18:29:12 +0530

    Add files via upload

---
+++ arduino2_code.ino
@@ -0,0 +1,220 @@
+#define trigPin1 8
+#define echoPin1 A2    
+#define trigPin2 11
+#define echoPin2 A3
+#define trigPin3 12
+#define echoPin3 A1
+#define mfront 3
+#define mback 6
+#define mleft 9
+#define mright 10
+#define seat_belt 5 
+
+String voice;
+
+
+int smokeA0 = A0;   //alcohol
+
+const int left_right_set=25;
+const int setspeed=153;
+int a=setspeed;
+char ch,state,c;
+const int setdistance=150;
+const int safedistance=90;
+const int criticaldistance=70;
+const int headlight=2;
+const int backlight=4;
+long duration, distance, RightSensor,BackSensor,FrontSensor,LeftSensor;
+
+
+      void setup(){
+              Serial.begin (9600);
+              pinMode(headlight, OUTPUT);
+              pinMode(backlight, OUTPUT);
+              pinMode(trigPin1, OUTPUT);
+              pinMode(echoPin1, INPUT);
+              pinMode(trigPin2, OUTPUT);
+              pinMode(echoPin2, INPUT);
+              pinMode(trigPin3, OUTPUT);
+              pinMode(echoPin3, INPUT);
+              pinMode(mfront, OUTPUT);
+              pinMode(mback, OUTPUT);
+              pinMode(mleft, OUTPUT);
+              pinMode(mright, OUTPUT);
+              pinMode(smokeA0, INPUT);
+              
+                  }
+
+      void loop(){  
+            digitalWrite(backlight,LOW);
+           
+            int seat=digitalRead(seat_belt);
+              while(Serial.available()>0)
+              {                //voice
+                   delay(10);
+                    char c=Serial.read();
+                    if(c=="engine on")
+                      {break; }
+                     voice += c;
+              }
+             if(voice=="engine on")
+             {
+               
+               digitalWrite(headlight,HIGH);
+               checkdistance(trigPin3, echoPin3);
+               FrontSensor = distance;
+               Serial.print(FrontSensor);
+               Serial.print("cm\n");
+               
+            if(Serial.available()>0){
+                 ch=(char) Serial.read();
+                 Serial.print(ch);
+             }
+             // Manual Mode
+          while(ch == 'a' ){                                                  //While1 Open
+                   if(Serial.available()>0){
+                     state = (char) Serial.read();  
+                     if(state == 'b'){
+                     ch = state;
+                      }
+                   }
+             else if(state == 'u'){
+                  Serial.print("Up Up Up Up Up Up Up Up"); //Up Control
+                  Serial.print(state);
+                  analogWrite(mfront, setspeed);   
+                  analogWrite(mback,setspeed);a=setspeed;
+                  digitalWrite(backlight,LOW);
+                }
+             else if(state == 's'){
+                  Serial.print("Stop Stop Stop Stop Stop"); //Stop
+                  Serial.print(state);
+                  digitalWrite(mfront, 0); 
+                  digitalWrite(mback,0); 
+                  digitalWrite(mleft, 0);   
+                  digitalWrite(mright,0); a=mfront;
+                  digitalWrite(backlight,HIGH);
+                  }
+            else if(state == 'l'){
+                 Serial.print("Left Left Left Left Left");    //Left Control
+                 Serial.print(state);
+                 digitalWrite(mfront, 0);  
+                 analogWrite(mback,setspeed); a=mback;
+                  digitalWrite(backlight,LOW);
+                  }
+            else if(state ==  'r'){
+                 Serial.print("Right Right Right Right Right");   //Right Control
+                 Serial.print(state);                                     
+                 analogWrite(mfront, setspeed);  
+                 digitalWrite(mback,0);    a=mback;
+                  digitalWrite(backlight,LOW);
+                  }
+            else if(state == 'd') {
+                Serial.print("Down Down Down Down Down");       //Down Control
+                Serial.print(state);                             
+                analogWrite(mleft, setspeed);   
+                analogWrite(mright,setspeed);a=mleft;   
+                  digitalWrite(backlight,LOW);
+                  }
+    }                                                                    //While1 Close
+    
+          //Acc Mode
+            if(Serial.available()>0){
+                 ch=(char) Serial.read();
+                 Serial.print(ch);
+             }
+                     if(ch == 'b'){                                           // Acc Loop Open
+                        
+                        checkdistance(trigPin1, echoPin1);
+                        RightSensor = distance;
+                        checkdistance(trigPin2, echoPin2);
+                        LeftSensor = distance;
+                        checkdistance(trigPin3, echoPin3);
+                        FrontSensor = distance;
+                        Serial.print(FrontSensor);
+                        Serial.print("cm\n");
+                            if(FrontSensor>110){ 
+                             analogWrite(mfront, setspeed);   
+                             analogWrite(mback,setspeed);
+                           // analogWrite(mleft, setspeed);   
+                           // analogWrite(mright,setspeed);
+                           digitalWrite(backlight,LOW); 
+                            a=setspeed; 
+                        }
+                 else if(FrontSensor>setdistance)                 //actual distance is more  accelerate
+                {
+                    while(FrontSensor!=setdistance){
+                         for(int i=a;i<=153;i++){
+                              a=i;
+                              analogWrite(mfront, a);   
+                              analogWrite(mback, a);  
+                            //  analogWrite(mleft, a);
+                            digitalWrite(backlight,LOW);   
+                            }
+                            break;
+                      } 
+                  }
+                 else{                                             //actual distance is more  deaccelerate
+                    while(FrontSensor!=setdistance){
+                      for(int i=a;i>=127;--i){
+                           a=i;
+                          analogWrite(mfront, a);   
+                          analogWrite(mback, a); 
+                         // analogWrite(mleft, a);   
+                           // analogWrite(mright,a);  
+                           
+                        }
+                        break;
+                    }
+                     if(FrontSensor<=criticaldistance){              //close proximity alert
+                        analogWrite(mfront,0);
+                         analogWrite(mright,0);
+                         analogWrite(mleft, 0);   
+                         analogWrite(mback,0);
+                         digitalWrite(backlight,HIGH);
+                      }
+                 }
+                 if(LeftSensor<left_right_set)                      //left and right sensor 
+                      {
+                         digitalWrite(backlight,HIGH);
+                        Serial.print("cut-in left");
+                         Serial.print("DRIVER INTERVENTION NEEDED");
+                         if(FrontSensor<=15){              //stop if left
+                        analogWrite(mfront,0);
+                         analogWrite(mright,0);
+                         analogWrite(mleft, 0);   
+                         analogWrite(mback,0);
+                         digitalWrite(backlight,HIGH);
+                      }
+                     
+                      }
+                      if(RightSensor<left_right_set)
+                      {
+                         digitalWrite(backlight,HIGH);
+                        Serial.print("cut-in right");
+                         Serial.print("DRIVER INTERVENTION NEEDED");
+                         if(FrontSensor<=15){              //stop if right
+                        analogWrite(mfront,0);
+                         analogWrite(mright,0);
+                         analogWrite(mleft, 0);   
+                         analogWrite(mback,0);
+                         digitalWrite(backlight,HIGH);
+                      }
+                      }
+           }                                                          //Acc loop close
+
+      }                                                                //voice close
+     /* if(c!='h')      {
+        Serial.print("voice recognition failed");
+      }*/                                                          
+      }                                                                //loop close
+void checkdistance(int trigPin,int echoPin)
+{
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+duration = pulseIn(echoPin, HIGH);
+distance = (duration/2) / 29.1;
+
+}
---
+++ node_mcu code.ino
@@ -0,0 +1,144 @@
+#include <DHT.h>  
+#include <SoftwareSerial.h>
+ #include <ESP8266WiFi.h>  
+ #include <WiFiClient.h>  
+ #include <ThingSpeak.h>  
+ #define DHTPIN D5  
+ #define encoder_pin  D2
+ #define DHTTYPE DHT11  
+ DHT dht(DHTPIN, DHTTYPE);  
+ const char* ssid = "iPhone";  
+ const char* password = "123456789";  
+ const int channelID = 297846;
+ String writeAPIKey = "LOUDHGMXFTSPYPPR";
+ const char* server = "api.thingspeak.com";
+ WiFiClient client;  
+ unsigned long myChannelNumber = 297846;  
+ const char * myWriteAPIKey = "LOUDHGMXFTSPYPPR";  
+ uint8_t temperature, humidity;  
+ float rpm;
+  String body="";
+  
+int b=0,c,i=0;
+
+ void setup()  
+ {  
+  Serial.begin(115200);  
+  dht.begin();  
+  delay(10);  
+  // Connect to WiFi network  
+  Serial.println();  
+  Serial.println();  
+  Serial.print("Connecting to ");  
+  Serial.println(ssid);  
+  WiFi.begin(ssid, password);  
+  while (WiFi.status() != WL_CONNECTED)  
+  {  
+   delay(500);  
+   Serial.print(".");  
+  }  
+  Serial.println("");  
+  Serial.println("WiFi connected");  
+  // Print the IP address  
+  Serial.println(WiFi.localIP());  
+  ThingSpeak.begin(client);  
+ }  
+ void loop()   
+ {  
+  static boolean data_state = false;  
+  temperature = dht.readTemperature();  
+  humidity = dht.readHumidity();  
+  Serial.print("Temperature Value is :");  
+  Serial.print(temperature);  
+  Serial.println("C");  
+  Serial.print("Humidity Value is :");  
+  Serial.print(humidity);  
+  Serial.println("%");  
+  // Write to ThingSpeak. There are up to 8 fields in a channel, allowing you to store up to 8 different  
+  // pieces of information in a channel. Here, we write to field 1.  
+  if( data_state )  
+  {  
+   ThingSpeak.writeField(myChannelNumber, 1, temperature, myWriteAPIKey);  
+   data_state = false;  
+  }  
+  else  
+  {  
+   ThingSpeak.writeField(myChannelNumber, 2, humidity, myWriteAPIKey);  
+   data_state = true;  
+  }  
+  delay(30000); // ThingSpeak will only accept updates every 15 seconds.  
+
+
+
+
+if (client.connect(server, 80))
+{
+// Measure Analog Input (A0)
+int x = analogRead(A0);
+// Construct API request body
+ body = "&field3=";
+body += String(x);
+
+//Serial.print("A0: ");
+//Serial.println(valueA0);
+client.print("POST /update HTTP/1.1\n");
+client.print("Host: api.thingspeak.com\n");
+client.print("Connection: close\n");
+client.print("X-THINGSPEAKAPIKEY: " + writeAPIKey + "\n");
+client.print("Content-Type: application/x-www-form-urlencoded\n");
+client.print("Content-Length: ");
+client.print(body.length());
+client.print("\n\n");
+client.print(body);
+client.print("\n\n");
+Serial.print(body);
+delay(30000);
+}
+
+
+if (client.connect(server, 80))
+{rpm=0;
+// Measure Analog Input (A0)
+for(i=3;i>0;i--)
+  { 
+    delay(1000);
+
+
+     
+    int a=digitalRead(encoder_pin);
+    if(a==1)
+    c=c+1;
+    Serial.println(i);
+  }
+ 
+   if(i==0)
+   {  rpm=c;
+      Serial.print("RPM = ");
+      Serial.println(rpm);
+      delay(200);
+      i=3;
+      
+   }  
+   
+// Construct API request body
+ body = "&field4=";
+body += String(rpm);
+
+//Serial.print("A0: ");
+//Serial.println(valueA0);
+client.print("POST /update HTTP/1.1\n");
+client.print("Host: api.thingspeak.com\n");
+client.print("Connection: close\n");
+client.print("X-THINGSPEAKAPIKEY: " + writeAPIKey + "\n");
+client.print("Content-Type: application/x-www-form-urlencoded\n");
+client.print("Content-Length: ");
+client.print(body.length());
+client.print("\n\n");
+client.print(body);
+client.print("\n\n");
+Serial.print(body);
+delay(30000);
+rpm=0;
+}
+  
+ }  
